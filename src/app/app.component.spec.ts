8e269ddd73d8025ef4d03d612e9ceeb9721c2bab
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2022-12-07 00:03:11 +0530

    test: fix tests for standalone components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
 import { TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
 
 import { AppComponent } from "./app.component";
-import { JsonConfigEditorComponent } from "./shared/json-config-editor/json-config-editor.component";
 
 describe("AppComponent", () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, JsonConfigEditorComponent],
-      declarations: [AppComponent],
-    }).compileComponents();
-  });
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     imports: [RouterTestingModule, JsonConfigEditorComponent],
+  //     declarations: [AppComponent],
+  //   }).compileComponents();
+  // });
 
   it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
